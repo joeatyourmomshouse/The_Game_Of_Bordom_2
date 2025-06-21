f825e643a2f911c207a8bd45ebfab72aa2b09749
Virat <viratmpriya@yahoo.com>
2025-06-20 19:04:19 -0700

    c

--- script.js
+++ script.js
@@ -1,42 +1,8 @@
-// Game intro in JavaScript alert form
-
-// Sequence of mysterious messages
-alert("Welcome, Traveler, to the Realm of Boredom...");
-
-// A brief pause for dramatic effect before the next alert
-setTimeout(function() {
-  alert("This is the second trial. Secrets have been hidden within the code itself.");
-}, 2000);
-
-setTimeout(function() {
-  alert("But beware, knowledge is a powerful force. The more you learn, the harder it becomes to escape...");
-}, 4000);
-
-setTimeout(function() {
-  alert("To navigate these cryptic realms, you must decrypt the puzzles and solve the riddles that await.");
-}, 6000);
-
-setTimeout(function() {
-  alert("The answers are right before your eyes... Can you see beyond the surface?");
-}, 8000);
-
-setTimeout(function() {
-  alert("Your mind will be tested. The path forward will twist and turn. Prepare yourself...");
-}, 10000);
-
-setTimeout(function() {
-  alert("Are you ready to face the challenge and continue your journey into the depths?");
-}, 12000);
-
-setTimeout(function() {
-  alert("...Or will you succumb to the all-consuming Boredom?");
-}, 14000);
-
-setTimeout(function() {
-  alert("Your first puzzle is about to begin. Brace yourself.");
-}, 16000);
-
-// Start the game or proceed with the next part
-setTimeout(function() {
-  alert("Begin.");
-}, 18000);
+var a = confirm("Ya got lost again?");
+if(a == true){
+    alert("Allow me to redirect you.");
+window.open('https://www.youtube.com/watch?v=y1fB7ztc1dQ', '_blank'); 
+}
+else{
+    
+}
