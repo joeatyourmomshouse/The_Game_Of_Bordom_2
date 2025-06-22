7918977c25e3134f832efaa069f92817f0cf2cd4
Virat <viratmpriya@yahoo.com>
2025-06-22 15:33:20 -0700

    d

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var MORE_INTO_IT = false;
 var THE_BETTER_END = false;
 var THE_DEEP_END = false;
 var GO_OUTSIDE_AND_TOUCH_SOME_GRASS = false;
+var EMOTIONAL_DAMAGE = false;
 
 // Game start
 var a = confirm("Ya got lost again? \nok = yes \ncancel = no");
@@ -23,7 +24,7 @@ if (a) {
 
     if (secretConfirm) {
       alert("Very well. The truth is...");
-      alert("You’ve always had the power. The question is... what will you do with it?");
+      alert("You’ve always had the power. The question is... what will you do with it? \nyou earned: more into it award");
       MORE_INTO_IT = true;
     } else {
       alert("Wise choice. Some doors are best left unopened.");
@@ -37,18 +38,29 @@ if (a) {
   var c = confirm("Now wasn't that fun?");
   if (c) {
     alert("Glad you can agree with me.");
-    alert("You won...\nCongrats, you got your first achievement!\nSIMPLE FUN");
+    alert("You won... \nyou earned: simple fun award");
     SIMPLE_FUN = true;
 
-    var don = confirm("Too simple though?");
+    var don = confirm("Whats that? Too easy?");
     if (don) {
-      alert("Fair enough. Try harder for the next one.");
+      alert("Fair enough.");
+        alert("Come back when you...");
+          window.open('https://www.harvard.edu/', '_blank');
+          window.open('https://www.spaceforce.com/apply-now', '_blank');
+           window.open('https://www.spaceforce.com/apply-now', '_blank');
+            window.open('https://www.goarmy.com/apply', '_blank');
+            alert("very funny, you just closed all the tabs and came back expecting more? \nyou earned: emotional damage award");
+     
+            
+
+        
+            
     }
   } else {
     alert("Guess I’ll have to try harder to impress you, hm?");
     var f = confirm("Are you sure?");
     if (f) {
-      for (var i = 0; i < 3; i++) {
+      for (var i = 0; i < 30; i++) {
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
       }
     } else {
@@ -69,7 +81,8 @@ if (SIMPLE_FUN) earned.push("SIMPLE FUN");
 if (MORE_INTO_IT) earned.push("MORE INTO IT");
 if (THE_BETTER_END) earned.push("THE BETTER END");
 if (THE_DEEP_END) earned.push("THE DEEP END");
-
+if (GO_TOUCH_SOME_GRASS) earned.push("GO TOUCH SOME GRASS");
+if (EMOTIONAL_DAMAGE) earned.push("EMOTIONAL DAMAGE");
 if (earned.length > 0) {
   alert("Achievements earned:\n" + earned.join("\n"));
   window.close();
